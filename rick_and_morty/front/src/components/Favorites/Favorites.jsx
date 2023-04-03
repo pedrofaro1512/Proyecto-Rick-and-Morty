7588026a269308ed2b6bf2b86eb8d85a27dfbf1b
Pedro Romero <108835699+pedrofaro1512@users.noreply.github.com>
2023-04-03 12:18:53 -0500

    Proyecto Rick
    Arreglo de favoritos

--- rick_and_morty/front/src/components/Favorites/Favorites.jsx
+++ rick_and_morty/front/src/components/Favorites/Favorites.jsx
@@ -1,11 +1,18 @@
-import { useSelector } from "react-redux"; 
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"; 
+import { getFavorites } from "../../redux/actions";
 import Card from "../Card/Card";
 import styles from './Favorite.module.css'
 
 
 const Favorites = () => {
+    const dispatch = useDispatch();
 
     const favorites = useSelector((state) => state.myFavorites);
+
+    useEffect(() => {
+        dispatch(getFavorites());
+    }, []);
 
     return(
         <div className={styles.divFavorite}>
--- rick_and_morty/front/src/redux/reducer.js
+++ rick_and_morty/front/src/redux/reducer.js
@@ -8,15 +8,15 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_FAVORITE:
-            return { ...state, myFavorites: [...state.myFavorites, action. payload]};
+            return { ...state, myFavorites: [...state.myFavorites, action. payload],};
         case REMOVE_FAVORITE:
             return { ...state, myFavorites: state.myFavorites.filter((char) => char.id !== action.payload),};
         case GET_CHARACTER_DETAIL:
-            return { ...state, characterDetail: action.payload};
+            return { ...state, characterDetail: action.payload,};
         case CLEAN_DETAIL:
-            return { ...state, characterDetail: {}};
+            return { ...state, characterDetail: {},};
         case GET_FAVORITES:
-            return { ...state, myFavorites: action.payload };
+            return { ...state, myFavorites: action.payload};
         
         default:
             return { ...state }
